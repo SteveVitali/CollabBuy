6b645a35c22c7fada47cd7490cd254f861ee528f
Steve Vitali <14stevevitali@gmail.com>
2014-02-14 21:06:08 -0500

    Initial Commit

---
+++ PennApps2014s/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PennApps2014s
+//
+//  Created by Steve John Vitali on 2/14/14.
+//  Copyright (c) 2014 Steve John Vitali. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ PennApps2014sTests/PennApps2014sTests.m
@@ -0,0 +1,34 @@
+//
+//  PennApps2014sTests.m
+//  PennApps2014sTests
+//
+//  Created by Steve John Vitali on 2/14/14.
+//  Copyright (c) 2014 Steve John Vitali. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PennApps2014sTests : XCTestCase
+
+@end
+
+@implementation PennApps2014sTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
