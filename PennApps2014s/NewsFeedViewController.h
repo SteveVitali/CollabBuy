c6b1dbbc89fc549d2815106012cbc860a5fa94e3
Steve Vitali <14stevevitali@gmail.com>
2014-02-16 02:59:54 -0500

    Add NewsFeedViewController

---
+++ PennApps2014s/NewsFeedViewController.h
@@ -0,0 +1,15 @@
+//
+//  NewsFeedViewController.h
+//  PennApps2014s
+//
+//  Created by Steve John Vitali on 2/16/14.
+//  Copyright (c) 2014 Steve John Vitali. All rights reserved.
+//
+
+#import "ListFriendItemsViewController.h"
+
+@interface NewsFeedViewController : ListFriendItemsViewController
+
+
+
+@end
